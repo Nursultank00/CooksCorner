b624b1d4fa6c5f14622ecadfda3910749bf6aa17
Nursultan Kozhogulov <n.kozhogulov@tcsbank.ru>
2024-03-17 23:57:11 +0300

    Add follow and unfollow views

--- userprofile/urls.py
+++ userprofile/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 
-from .views import UserProfileAPIView
+from .views import (
+                    UserProfileAPIView,
+                    UserFollowAPIView,
+                    UserUnfollowAPIView
+)
 
 urlpatterns = [
     path('<slug:slug>/', UserProfileAPIView.as_view(), name = 'profile-detail'),
+    path('<slug:slug>/follow/', UserFollowAPIView.as_view(), name = 'follow-user'),
+    path('<slug:slug>/unfollow/', UserUnfollowAPIView.as_view(), name = 'unfollow-user'),
 ]
--- userprofile/views.py
+++ userprofile/views.py
@@ -3,7 +3,7 @@ from rest_framework.permissions import IsAuthenticated
 from drf_yasg.utils import swagger_auto_schema
 
 from .serializers import ProfileSerializer
-from .models import UserProfile
+from .models import UserProfile, User
 
 # Create your views here.
 
@@ -47,8 +47,56 @@ class UserProfileAPIView(APIView):
         except Exception:
             return Response({'Error':'User profile is not found.'}, status=status.HTTP_404_NOT_FOUND)
         serializer = ProfileSerializer(profile, data = request.data)
-        # serializer.is_valid(raise_exception=True)
         if serializer.is_valid():
             serializer.update(profile, serializer.validated_data)
             return Response({'Message':'User profile is successfully updated.'}, status=status.HTTP_200_OK)
         return Response({'Error':'Invalid data.'}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserFollowAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    @swagger_auto_schema(
+        tags=['User profile'],
+        operation_description="Этот эндпоинт предоставляет "
+                              "возможность начать "
+                              "отслежить другого "
+                              "пользователя.",         
+        responses = {
+            200: "Success.",
+            400: "Invalid data.",
+            404: "User profile is not found.",
+        },
+    )
+    def put(self, request, slug, *args, **kwargs):
+        try:
+            profile = UserProfile.objects.get(slug = slug)
+        except Exception:
+            return Response({'Error':'User profile is not found.'}, status=status.HTTP_404_NOT_FOUND)
+        user = request.user
+        user.profile.following.add(profile.user)
+        return Response({'Message':'Success.'}, status=status.HTTP_200_OK)
+    
+class UserUnfollowAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    @swagger_auto_schema(
+        tags=['User profile'],
+        operation_description="Этот эндпоинт предоставляет "
+                              "возможность перестать "
+                              "отслежить другого "
+                              "пользователя.",         
+        responses = {
+            200: "Success.",
+            400: "Invalid data.",
+            404: "User profile is not found.",
+        },
+    )
+    def put(self, request, slug, *args, **kwargs):
+        try:
+            profile = UserProfile.objects.get(slug = slug)
+        except Exception:
+            return Response({'Error':'User profile is not found.'}, status=status.HTTP_404_NOT_FOUND)
+        user = request.user
+        user.profile.following.remove(profile.user)
+        return Response({'Message':'Success.'}, status=status.HTTP_200_OK)
